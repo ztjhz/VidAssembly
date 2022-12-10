ecba1a793a9f28eb96ca6a738f1ba6ee822e8937
Jing Hua <tohjinghua123@gmail.com>
2022-12-10 20:15:05 +0800

    video_preprocessing

---
+++ backend/lib/video_preprocessing.py
@@ -0,0 +1,17 @@
+from glob import glob
+import os
+import subprocess
+
+
+def speedup_1_6x(work_dir: str) -> str:
+    file_in = glob(os.path.join(work_dir, 'video.*'))[0]
+    file_out = file_in.replace('video.', 'preprocessed.')
+
+    _, filename_preprocessed = os.path.split(file_out)
+
+    subprocess.run(('ffmpeg', '-y', '-i', file_in, '-filter_complex',
+                    '[0:v]setpts=0.625*PTS[v];[0:a]atempo=1.6[a]', '-map',
+                    '[v]', '-map', '[a]', file_out),
+                   check=True)
+
+    return filename_preprocessed
